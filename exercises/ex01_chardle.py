4d25a91dd6ce6262294f976b849f190a17546cb6
Luke McCain <lmccain@unc.edu>
2023-01-21 15:47:36 -0500

    Progess on Exercise 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -10,7 +10,7 @@ if len(character) != 1:
     exit(print("Error: Character must be a single character."))
 matches: int = 0
 
-print("Searching for " + character + " in " + word )
+print("Searching for " + character + " in " + word)
 
 if character == word[0]:
     matches = matches + 1
@@ -29,15 +29,15 @@ if character == word[4]:
     print(character + " found at index 4")
 
 if matches == 0:
-    print("No instances of " + character + " found in " + word )
+    print("No instances of " + character + " found in " + word)
 if matches == 1:
-    print("1 instance of " + character + " found in " + word )
+    print("1 instance of " + character + " found in " + word)
 if matches == 2:
-    print("2 instances of " + character + " found in " + word )
+    print("2 instances of " + character + " found in " + word)
 if matches == 3:
-     print("3 instances of " + character + " found in " + word )
+     print("3 instances of " + character + " found in " + word)
 if matches == 4:
-     print("4 instances of " + character + " found in " + word )
+     print("4 instances of " + character + " found in " + word)
 if matches == 5:
-     print("5 instances of " + character + " found in " + word )
+     print("5 instances of " + character + " found in " + word)
 

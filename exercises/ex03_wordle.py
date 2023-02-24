fa5a09863c361310fcc59fde37c1c91f125cf86d
Luke McCain <lmccain@unc.edu>
2023-02-24 15:55:13 -0500

    ex03 final product

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -1,6 +1,7 @@
-"""Real Wordle"""
+"""Real Wordle."""
 
 __author__ = "730486310"
+
 
 def contains_char(word_being_searched: str, character_being_searched_for: str) -> bool:
     """Searching for character in any index of the given word."""
@@ -11,6 +12,7 @@ def contains_char(word_being_searched: str, character_being_searched_for: str) -
             return True
         i = i + 1
     return False
+
 
 def emojified(guess: str, secret: str) -> str:
     """Given two strings of equal length, a green, yellow, or white box will appear based on the guess/secret, like in Wordle."""
@@ -24,18 +26,13 @@ def emojified(guess: str, secret: str) -> str:
         if guess[i] == secret[i]:
             s = s + GREEN_BOX
         else:
-            alternate: int = 0
-            character_exist: bool = False
-            while not character_exist and alternate < len(secret):
-                if guess[i] == secret[alternate]:
-                    character_exist: bool = True
-                    s = s + YELLOW_BOX
-                else:
-                    alternate = alternate + 1
-            if character_exist == False:
+            if contains_char(secret, guess[i]):
+                s = s + YELLOW_BOX
+            else:
                 s = s + WHITE_BOX
         i = i + 1
     return s
+
 
 def input_guess(expected_length: int) -> str:
     """User will be prompted to insert a word of the correct character length, until successful."""
@@ -44,26 +41,27 @@ def input_guess(expected_length: int) -> str:
         return word
     while expected_length != len(word):
         word = input(f"That wasn't {expected_length} chars! Try again: ")
-        if len(word) == expected_length:
-            return word
+    return word
+        
 
 def main() -> None:
     """The entry point of the program and main game loop."""
     secret_word: str = "codes"
     turns: int = 1
     success: bool = False
-    while turns < 7 and success == False:
+    while turns < 7 and not success:
         print(f"=== Turn {turns}/6 ===")
         guess: str = (input_guess(5))
-        print(emojified (guess, secret_word))
+        print(emojified(guess, secret_word))
         if guess == secret_word:
             print(f"You won in {turns}/6 turns!")
             success = True
         else:
             turns = turns + 1
             success = False
-    if turns == 7 and success == False:
+    if turns == 7 and not success:
         print("X/6 - Sorry, try again tomorrow!")
+
 
 if __name__ == "__main__":
     main()

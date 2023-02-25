0a54731fcdd8f28e12d8300e53d26adc4ef82bb9
Luke McCain <lmccain@unc.edu>
2023-02-25 14:30:31 -0500

    second submission

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -3,41 +3,37 @@
 __author__ = "730486310"
 
 
-def all(list_of_int: list[int], given_int: int) -> bool:
+def all(input: list[int], integer: int) -> bool:
+    """Testing if all integers in list are equal to the given int."""
     i: int = 0
-    while i < len(list_of_int):
-        if given_int == list_of_int[i]:
+    while i < len(input):
+        if integer == input[i]:
             i += 1
         else:
-            if list_of_int == list():
+            if input == list():
                 return False
             return False
     return True
 
-    
-int_list_for_all: list[int] = [1,1,1]
-integer_for_all: int = 1
-print(all(int_list_for_all, integer_for_all))
 
-def max(list_of_int: list[int]) -> int:
+def max(input: list[int]) -> int:
+    """Trying to find the max int in a list of int."""
     if len(input) == 0:
-        raise ValueError("max() arg is an empty list")
+        raise ValueError("max() arg is an empty List")
     i: int = 0
     idx: int = 0
     max: int = 0
-    while i < len(list_of_int):
-        if list_of_int[i] >= list_of_int[idx]:
-            max == list_of_int[i]
+    while i < len(input):
+        if input[i] >= input[idx]:
+            max == input[i]
         else:
             idx += 1
         i += 1
     return max
 
-integer_list_for_max: list[int] = [1,3,2]
-print(max(integer_list_for_max))
-
 
 def is_equal(list_of_int: list[int], second_list_of_int: list[int]) -> bool:
+    """Testing if every int at every index is equal to eachother."""
     i: int = 0
     while i < len(list_of_int) and i < len(second_list_of_int):
         if list_of_int[i] == second_list_of_int[i]:
@@ -45,7 +41,3 @@ def is_equal(list_of_int: list[int], second_list_of_int: list[int]) -> bool:
         else:
             return False
     return True
-
-int_list_for_is_equal: list[int] = [1,0,1]
-second_list_of_int_for_is_equal: list[int] = [1,0,1]
-print(is_equal(int_list_for_is_equal, second_list_of_int_for_is_equal))

8e3963860bf15973c4cb8aa271d65db8f7b65a9d
Luke McCain <lmccain@unc.edu>
2023-03-23 16:00:06 -0400

    hi

--- lessons/notes.py
+++ lessons/notes.py
@@ -1,4 +1,9 @@
-my_l: list[str] = ["unicycle", "bicycle", "helicopter", "scooter"]
-
-for x in my_l:
-    print(x)
+def stink(inp_list: dict[str, int]) -> list[int]:
+    """Returns list of numbers from dict that are even and less than 11."""
+    ret_list: list[int] = []
+    for key in inp_list:
+        if inp_list[key] % 2 == 0 and inp_list[key] < 11:
+            ret_list.append(inp_list[key])
+        return ret_list
+    
+print(stink({"vanilla":8 ,"chocolate":10 }))
